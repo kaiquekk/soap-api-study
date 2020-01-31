e071c0ce5a434fbdd5287a140371fb100bb01f95
Kaique Kazuyoshi Komata <kaique.komata@opus-software.com.br>
2020-01-31 13:48:58 -0300

    Add simple views and CC checker API call
    Signed-off-by: Kaique Kazuyoshi Komata <kaique.komata@opus-software.com.br>

--- src/app.js
+++ src/app.js
@@ -4,23 +4,30 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.set('views', './views');
+app.set('view engine', 'pug');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 function mainRoute() {
     const main = new express.Router();
+    main.all('/', function(req, res) {
+        res.render('main');
+    });
 
-    main.all('/', function (req, res) {
+    main.all('/check', function (req, res) {        
         const wsdlUrl = 'https://ws.cdyne.com/creditcardverify/luhnchecker.asmx?wsdl';
-        const number = { CardNumber: '4111111111111111' };
+        const number = { CardNumber: req.body.number };
         soap.createClient(wsdlUrl, function (err, client) {
             if (err) return res.status(500).json(err);
             client.CheckCC(number, function (err, result) {
                 if (err) return res.status(500).json(err);
-                const type = result.cardType;
-                const valid = result.cardValid;
-                return res.json(result);
+                const type = result.CheckCCResult.CardType;
+                let valid;
+                if(result.CheckCCResult.CardValid) valid = 'Yes';
+                else valid = "No";
+                res.render('check', {type:type, valid:valid});                
             });
         });
     });
